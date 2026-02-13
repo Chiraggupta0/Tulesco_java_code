239075966e9187a7c754f9496b4f3fef54f62678
CHIRAG GUPTA <chiraggupta0963@gmail.com>
2026-02-13 14:41:39 +0530

    added

---
+++ core_java/Thread/_10_52.java
@@ -0,0 +1,36 @@
+package core_java.Thread;
+
+public class _10_52 {
+    public static void main(String[] args) {
+        Runnable obj1 = () ->
+        {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("hi");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        
+
+        Runnable obj2 = () ->
+        {
+            for (int i = 0; i < 10; i++) {
+                System.out.println("hello");
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Thread t1= new Thread(obj1);
+        Thread t2= new Thread(obj2);
+
+        t1.start();
+        t2.start();
+    }
+}
